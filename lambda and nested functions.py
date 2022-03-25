cd15924e533c6a66fa78c3ae41d781231a8ceb22
bro_code <78599959+MoriKeli@users.noreply.github.com>
2022-03-25 12:50:25 +0000

    Add files via upload

---
+++ lambda and nested functions.py
@@ -0,0 +1,16 @@
+def hello():
+    x = 'Mori'
+
+    def add_nums(y, z):
+        sum_num = y+z
+        return sum_num
+    print(add_nums(2, 3))
+
+    # Lambda function
+    # func = lambda y, z: y+z
+    # print(func(2, 3))
+
+    return 'Hello, '+x+'!'
+
+
+print(hello())
