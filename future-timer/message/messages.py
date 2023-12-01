152b5eb8e41f531700a1ef55fcbada16327fbbca
brocode <kelimbth@gmail.com>
2023-12-01 12:25:08 +0300

    messages module

---
+++ future-timer/message/messages.py
@@ -0,0 +1,10 @@
+
+def error_message(message_str):
+    print('==='*30)
+    print(f'{message_str:^65s}')
+    print('==='*30)
+
+def prompt_message(message_str):
+    print('==='*30)
+    print(f'{message_str:<30s}')
+    print('==='*30)
